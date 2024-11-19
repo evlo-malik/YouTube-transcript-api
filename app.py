b8a135983737f376b5879492623f2f84d079153b
Малик Евлоев <malikevloev@MacBook-Pro-Malik.local>
2024-11-19 20:02:05 +0000

    Prepare app for deployment

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify
 from youtube_transcript_api import YouTubeTranscriptApi
 import re
+import os
+
 
 app = Flask(__name__)
 
@@ -33,4 +35,7 @@ def get_transcript():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
+
+
